2cda9a4fc073e553486beda56f9b4f546290bbe6
Chris Gomes <ugali_guy@hotmail.com>
2015-10-21 21:55:42 -0500

    Initial commit

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,25 @@
+# Math Methods
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ sum = num_1 + num_2
+ sum
+end
+
+def subtract(num_1, num_2)
+ diff = num_1 - num_2
+ diff
+end
+
+def multiply(num_1, num_2)
+ prod = num_1 * num_2
+ prod
+end
+
+def divide(num_1, num_2)
+ quot = num_1.to_f / num_2
+end
