d47ff195cc82762ff1b72be55a84527a7678ad8a
Chris Gomes <ugali_guy@hotmail.com>
2015-11-24 13:22:24 -0600

    Initial commit

---
+++ week-9/dom-manipulation/home_page.js
@@ -0,0 +1,40 @@
+// DOM Manipulation Challenge
+
+
+// I worked on this challenge with Jon Clayton.
+
+
+// Add your JavaScript calls to this page:
+
+// Release 0:
+
+
+document.getElementById("release-0").className = "done";
+
+
+
+// Release 1:
+document.getElementById("release-1").style.display = "none";
+
+
+// Release 2:
+document.getElementsByTagName("H1")[0].innerHTML = "I completed release 2.";
+
+
+// Release 3:
+document.getElementById("release-3").style.backgroundColor = "#955251";
+
+
+
+// Release 4:
+var x = document.getElementsByClassName("release-4");
+var i;
+for (i = 0; i < x.length; i++) {
+    x[i].style.fontSize = "2em";
+}
+
+
+
+// Release 5:
+var tmpl = document.getElementById('hidden');
+document.body.appendChild(tmpl.content.cloneNode(true));
